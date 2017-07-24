2759f75f33fa91391861141e0a1e40240c4535e7
Naman Gupta <namgupta96@gmail.com>
2017-07-24 09:00:52 +0530

    written code for movement

--- 2048.cpp
+++ 2048.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+#include<conio.h>
+#include <windows.h>
 //#include <iostream>
 using namespace std;
 
@@ -64,17 +66,138 @@ bool swamp()
         return true;
     }
 }
+void move_up()
+{
+    for(int i=1;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            int I=i,J=j;
+            if(mat[i][j]!=0)
+            {
+                while(mat[I-1][J]==0 && I!=0)
+                {
+                    mat[I-1][J]=mat[I][J];
+                    mat[I][J]=0;
+                    I--;
+                }
+                if(mat[I-1][J]==mat[I][J])
+                {
+                    mat[I-1][J]=mat[I][J]+mat[I][J];
+                    mat[I][J]=0;
+                }
+            }
+        }
+    }
+}
+void move_down()
+{
+    for(int i=3;i>=0;i--)
+    {
+        for(int j=0;j<4;j++)
+        {
+            int I=i,J=j;
+            if(mat[i][j]!=0)
+            {
+                while(mat[I+1][J]==0 && I!=3)
+                {
+                    mat[I+1][J]=mat[I][J];
+                    mat[I][J]=0;
+                    I++;
+                }
+                if(mat[I+1][J]==mat[I][J])
+                {
+                    mat[I+1][J]=mat[I][J]+mat[I][J];
+                    mat[I][J]=0;
+                }
+            }
+        }
+    }
+}
+void move_left()
+{
+    for(int i=0;i<4;i++)
+    {
+        for(int j=1;j<4;j++)
+        {
+            int I=i,J=j;
+            if(mat[i][j]!=0)
+            {
+                while(mat[I][J-1]==0 && J!=0)
+                {
+                    mat[I][J-1]=mat[I][J];
+                    mat[I][J]=0;
+                    J--;
+                }
+                if(mat[I][J-1]==mat[I][J])
+                {
+                    mat[I][J-1]=mat[I][J]+mat[I][J];
+                    mat[I][J]=0;
+                }
+            }
+        }
+    }
+}
+void move_right()
+{
+    for(int i=0;i<4;i++)
+    {
+        for(int j=3;j>=0;j--)
+        {
+            int I=i,J=j;
+            if(mat[i][j]!=0)
+            {
+                while(mat[I][J+1]==0 && J!=3)
+                {
+                    mat[I][J+1]=mat[I][J];
+                    mat[I][J]=0;
+                    J++;
+                }
+                if(mat[I][J+1]==mat[I][J])
+                {
+                    mat[I][J+1]=mat[I][J]+mat[I][J];
+                    mat[I][J]=0;
+                }
+            }
+        }
+    }
+}
+void movement(char n)
+{
+    switch(n)
+    {
+    case 'w':move_up();
+        break;
+    case 's':move_down();
+        break;
+    case 'a':move_left();
+        break;
+    case 'd':move_right();
+        break;
+    }
+
+}
 int main()
 {
+    char n;
     for(;;)
     {
-        show_mat();
-        cout<<endl;
         if(!swamp())
         {
             cout<<"\nGame over";
             return 0;
         }
-
+        show_mat();
+        cout<<endl;
+        while(!kbhit())
+        {
+            n=getch();
+            break;
+        }
+        movement(n);
     }
 }
+/*  to add:-
+1. no swamp for no movement
+
+*/
---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int te(int n)
+{
+    if(n==10)
+    {
+        return 1;
+    }
+    else
+        return 0;
+
+}
+int main()
+{
+    if(!te(11))
+    {
+        cout<<"yo"<<endl;
+    }
+    if(te(10))
+    {
+        cout<<"10"<<endl;
+    }
+    return 0;
+}
